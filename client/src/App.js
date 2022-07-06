d778713c11abf20c0ce379ee7c75b24875bffba8
meriab21 <meriab1234@gmail.com>
2022-07-06 20:42:11 +0300

    initialized react project

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+// import React, { Component } from 'react';
+// import {SearchBox} from './components/search-box';
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+      
+          <button >
+            Load text
+          </button>
+          
+          <button>
+              Read text
+          </button>
+         
+      </header>
+    </div>
+  );
+}
+
+export default App;
