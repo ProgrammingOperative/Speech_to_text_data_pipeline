c582511bf6e0d9cf7889c1a843486cd8a2f927fd
waCira <wachura11t@gmail.com>
2022-07-08 17:13:22 +0300

    setting necessary locations

--- airflow/dags/dag.py
+++
@@ -1,9 +0,0 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator 
-from random import randint
-
-print('dag imports are working')
-
-def _training_model():
-    return randint(1,10)
-
